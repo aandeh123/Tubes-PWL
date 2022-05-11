e2aee7dbc9fe2d38226378bac3028e17ff57afec
Clarisa 119140143 <83949360+MOMOtheRedApple@users.noreply.github.com>
2022-05-11 17:19:11 +0700

    Add files via upload

---
+++ user/admin/aspirasi/cekkode/cekkode/header.php
@@ -0,0 +1,12 @@
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<div class="page-header">
+	<a href="../../../../"><img src="../../../../images/logoitera.jpg" width="140px" height="100px"></a><a href="../../../../"><font size=1><br></font>e-Aspirasi</a>
+        <h4><font>Himpunan Mahasiswa Teknik Informatika<br>
+        Institut Teknologi Sumatera</font></h4><br>
+</div>
+</div>        
+</div>
+</div>
+	  
