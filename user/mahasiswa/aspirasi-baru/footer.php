98bd91b2ea5368255563b45a4535f236d6bd129a
Agusto Hawlai <64683138+agustohr@users.noreply.github.com>
2022-05-11 20:55:27 +0700

    Delete user/mahasiswa directory

--- user/mahasiswa/aspirasi-baru/footer.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-<div class="row">
-	<div class="col-xs-12 col-md-12 col-sm-12 col-lg-12">
-		<div class="page-footer">
-			<p>&copy; 2022 by Himpunan Mahasiswa Teknik Informatika.</p>
-		</div>
-	</div>	
-</div>
-</div>
