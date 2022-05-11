986a23aa1bbe20c1e31f48da11b311501f40ea8c
Muhammad Fariz Luthfi <84334672+Ziraf420@users.noreply.github.com>
2022-05-11 15:51:39 +0700

    Commit TUBES PWL
    e-Aspirasi

---
+++ body.php
@@ -0,0 +1,15 @@
+﻿<br>
+<div class="container">
+<div class="row">
+	<div class="col-md-8">	
+		<div class="page-header-body">
+			<div class="text text-justify">
+			<h3>e-Aspirasi </h3>
+			
+			</div>
+		</div>		
+	</div>
+</div>
+</div>
+</div>
+<br>
